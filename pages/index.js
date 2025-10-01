e8d2c997bb0e08879aadda78f645dff096d692eb
sergiocampelo10 <sergioocf05@gmail.com>
2025-10-01 07:53:20 +0000

    alterar novamente o texto para o desafio do curso

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãe, obrigado por sempre me cobrar, me apoiar e acreditar em mim.<br></br>Sei que cada palavra sua vem do amore do seu desejo de me ver crescer.<br></br>Te amo!
+    return <h1>Mãe, obrigado por sempre me cobrar, me apoiar e acreditar em mim.<br></br>Sei que cada palavra sua vem do amore do seu desejo de me ver crescer.<br></br>Te amo!💕
         </h1>
 }
 
