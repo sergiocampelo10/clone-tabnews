6d2cff481b581a4eba5ba7254405f6c5388b8f9e
sergiocampelo10 <sergioocf05@gmail.com>
2025-10-01 06:21:35 +0000

    Alteração de texto do arquivo Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Página inicial</h1>
+    return <h1>Mãe, obrigado por sempre me cobrar, me apoiar e acreditar em mim.<br></br>Sei que cada palavra sua vem do amore do seu desejo de me ver crescer.<br></br>Te amo!
+        </h1>
 }
 
 export default Home;
