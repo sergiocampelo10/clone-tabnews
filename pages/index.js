413cb97896e38e3d06d31c56ccd0649e97d6feb8
sergiocampelo10 <sergioocf05@gmail.com>
2025-10-03 17:12:53 +0000

    adiciona scripts `lint:check` e ´lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 function Home() {
-    return <h1>Mãe, obrigado por sempre me cobrar, me apoiar e acreditar em mim.<br></br>Sei que cada palavra sua vem do amore do seu desejo de me ver crescer.<br></br>Te amo!💕
-        </h1>
+  return (
+    <h1>
+      Mãe, obrigado por sempre me cobrar, me apoiar e acreditar em mim.<br></br>
+      Sei que cada palavra sua vem do amore do seu desejo de me ver crescer.
+      <br></br>Te amo!💕
+    </h1>
+  );
 }
 
 export default Home;
